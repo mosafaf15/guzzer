f72edbdf53c81c697c19bf878df9b5e5600247aa
mosafaf15 <121920566+mosafaf15@users.noreply.github.com>
2024-05-21 04:00:14 -0500

    [Update spider.py] some 404 pages and 403 page warning

--- spider.py
+++ spider.py
@@ -5,25 +5,32 @@ import requests
 from difflib import SequenceMatcher
 
 
-def spider(base_url,):
+def spider(
+    base_url,
+):
     urls = []
-    if base_url[-1] != '/': base_url += '/'  #append "/" to the end of the base url
+    if base_url[-1] != "/":
+        base_url += "/"  # append "/" to the end of the base url
 
-    not_found = requests.get(base_url+"something_that_shows_this_is_a_not_found_page")
+    not_found = requests.get(base_url + "something_that_shows_this_is_a_not_found_page")
     not_found_text = not_found.text
 
-    print('finding interesting files...')
-    with open('common.txt','r') as wordlist:
+    print("finding interesting files...")
+    with open("common.txt", "r") as wordlist:
         for w in wordlist:
             word = w[:-1]
             url = base_url + word
             urls.append(url)
 
-    async def get_url(session, url):
+    async def get_url(session: aiohttp.ClientSession, url):
         async with session.get(url) as resp:
             resp_text = await resp.text()
-            if SequenceMatcher(None, not_found_text, resp_text).ratio() < 0.9 :
-                print('found this url, might be interesting:  '+ url)
+            status = resp.status
+            if SequenceMatcher(None, not_found_text, resp_text).ratio() < 0.9 and status != 404:
+                if status == 403:
+                    print(f"found this url, might be interesting: {url} [Permission denied 403]")
+                else:
+                    print(f"found this url, might be interesting: {url}")
 
     async def main():
 
@@ -36,7 +43,3 @@ def spider(base_url,):
             original_request = await asyncio.gather(*tasks)
 
     asyncio.run(main())
-
-
-
-
